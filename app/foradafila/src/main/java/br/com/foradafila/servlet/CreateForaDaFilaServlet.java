4e719c7b667c5146eb6dd0f3b6c7e65d191e2a59
Gabriel Parra <bielparra17@gmail.com>
2024-09-29 13:56:49 -0300

    correção de bugs relacionado a servlet

--- app/foradafila/src/main/java/br/com/foradafila/servlet/CreateForaDaFilaServlet.java
+++ app/foradafila/src/main/java/br/com/foradafila/servlet/CreateForaDaFilaServlet.java
@@ -1,17 +1,26 @@
 package br.com.carstore.servlet;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-@WebServlet("/create-car")
-public class ForaDaFilaServlet extends HttpServlet {
+@WebServlet("/fora-da-fila")
+public class CreateForaDaFilaServlet extends HttpServlet{
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        String carName = request.getParameter("car-name");
+        String nomeIngresso = request.getParameter("nome_ingresso");
+        String nIngresso = request.getParameter("n_ingresso");
 
-        System.out.println(carName);
+        System.out.println(nomeIngresso);
+        System.out.println(nIngresso);
 
         request.getRequestDispatcher("index.html").forward(request, response);
     }
+
+
 }
